e0454dc44926e4c180049898292cd1d503efd41b
mts508 <mathyasrodriguez@gmail.com>
2018-10-03 11:47:00 -0300

    initial comit

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('site').then(function(cache) {
+      return cache.addAll(
+        [
+          '/index.html',
+          '/mega.jpg',
+          '/yMJRMIlzdpvBhQQL_Qq7dy0.woff2'
+        ]
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
